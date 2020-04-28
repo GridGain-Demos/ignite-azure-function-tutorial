f37b5fb32ef477405b4435ea4fd3344a9e44aa94
Denis Magda <dmagda@gridgain.com>
2020-04-28 13:28:24 -0700

    prepared Azure Function source code template

--- src/main/java/org/gridgain/demo/azurefunction/functions/PopulationFunction.java
+++ src/main/java/org/gridgain/demo/azurefunction/functions/PopulationFunction.java
@@ -35,118 +35,17 @@ import org.apache.ignite.client.IgniteClient;
 import org.apache.ignite.configuration.ClientConfiguration;
 
 /**
- * Azure Functions with HTTP Trigger.
+ * An Azure Function with HTTP Trigger template.
  */
 public class PopulationFunction {
 
-    private static final Object monitor = new Object();
-
-    private static volatile IgniteClient thinClient;
-
-    //Change with the address of your Ignite Kubernetes Service
-    private static final String CLUSTER_IP = "13.87.164.252";
-
     /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     * The Azure Function that listens at endpoint "/api/population" and processes two types of requests.
      */
     @FunctionName("population")
     public HttpResponseMessage run(
             @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
-        context.getLogger().info("Java HTTP trigger processed a request.");
-
-        // Determining a type of request
-        String query = request.getQueryParameters().get("popGreaterThan");
-        String paramValue = request.getBody().orElse(query);
-
-        if (paramValue != null)
-            //Getting all the cities with population greater or equal to the specified one.
-            return getCitiesPopulationGreaterThan(Integer.valueOf(paramValue), request);
-
-        query = request.getQueryParameters().get("avgInCountry");
-        paramValue = request.getBody().orElse(query);
-
-        if (paramValue != null)
-            //Calculating average population in the country.
-            return getAvgPopulationInCountry(paramValue, request);
-
-        return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body(
-            "Not enough parameters are passed to complete your request").build();
-    }
-
-    private HttpResponseMessage getCitiesPopulationGreaterThan(int population, HttpRequestMessage<Optional<String>> request) {
-        SqlFieldsQuery sqlQuery = new SqlFieldsQuery("SELECT city.name, city.population, country.name " +
-            "FROM city JOIN country ON city.countrycode = country.code WHERE city.population >= ? " +
-            "GROUP BY country.name, city.name, city.population ORDER BY city.population DESC, city.name")
-            .setArgs(population);
-
-        List<List<?>> result = getThinClientConnection().query(sqlQuery).getAll();
-
-        HttpResponseMessage.Builder responseBuilder = request.createResponseBuilder(HttpStatus.OK);
-
-        responseBuilder.header("Content-Type", "text/html");
-
-        StringBuilder response = new StringBuilder("<html><body><table>");
-
-        for (List<?> row: result) {
-            response.append("<tr>");
-
-            for (Object column: row)
-                response.append("<td>").append(column).append("</td>");
-
-            response.append("</tr>");
-        }
-
-        response.append("</table></body></html>");
-
-        return responseBuilder.body(response.toString()).build();
-    }
-
-    private HttpResponseMessage getAvgPopulationInCountry(String countryCode, HttpRequestMessage<Optional<String>> request) {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-
-        HttpGet httpGet = new HttpGet("http://" + CLUSTER_IP +
-            ":8080/ignite?cmd=exe&name=org.gridgain.demo.azurefunction.compute.AvgCalculationTask&p1=" + countryCode);
-
-        CloseableHttpResponse response = null;
-
-        try {
-            response = httpClient.execute(httpGet);
-
-            HttpEntity entity1 = response.getEntity();
-
-            StringWriter writer = new StringWriter();
-            IOUtils.copy(entity1.getContent(), writer, StandardCharsets.US_ASCII);
-
-            return request.createResponseBuilder(HttpStatus.OK).body(writer.toString()).build();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return request.createResponseBuilder(HttpStatus.BAD_GATEWAY).body(
-                "Failed to execute the request: " + e.getMessage()).build();
-        }
-        finally {
-            try {
-                if (response != null)
-                    response.close();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private IgniteClient getThinClientConnection() {
-        if (thinClient == null) {
-            synchronized (monitor) {
-                ClientConfiguration cfg = new ClientConfiguration().setAddresses(CLUSTER_IP + ":10800");
-
-                thinClient = Ignition.startClient(cfg);
-            }
-        }
-
-        return thinClient;
+        return null;
     }
 }
